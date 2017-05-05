8dd0c4c837f0784d1478fb7122e19c57beac410e
Rosario Felix <elijan_11@hotmail.com>
2017-05-05 13:47:12 -0500

    subiendo parte uno

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",function() {
+
+var sec= document.getElementById('part-one');
+for(var i =0;i<=2;i++){
+  var div= document.createElement("div");
+  div.classList.add("one");
+  sec.appendChild(div);
+}
+});
