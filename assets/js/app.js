3b9e67c06457e6c2c536b7eb0b856c10d7750cd5
Rosario Felix <elijan_11@hotmail.com>
2017-05-05 14:38:08 -0500

    subiendo tercera parte

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,5 +7,9 @@ for(var i =0;i<=2;i++){
   var divDos= document.createElement("div");
   divDos.classList.add("two");
   document.getElementById('part-two').appendChild(divDos);
+
+  var divTres=document.createElement("div");
+  divTres.classList.add("three");
+  document.getElementById("part-three").appendChild(divTres);
 }
 });
